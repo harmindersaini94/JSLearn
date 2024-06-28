50b1a5a84a6adcec140b53a9984cfc07468fcc8f
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-06-28 23:39:22 +0000

    datatypes

---
+++ JSBasics/01_First.js
@@ -0,0 +1,14 @@
+console.log("Hi Harminder!")
+
+const rollNo = 123456;
+let myName = "Harminder"
+var jonDesc = "Software Developer";
+originCity = "Jalandhar"
+let currentCity;
+
+// Var vs Let
+/*
+Var does not follow the concept of block scope or functional scope. so if ypu declare a key word with var inside a function, it can be accessed outside as well
+Let is made to solve this as let follow the block scopw and is not accessible outside the block
+*/
+console.table([rollNo, myName, jonDesc, originCity, currentCity])
---
+++ JSBasics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // It means that all JS code is treated based on newer version
+
+//alert(3+3) not used in node.js but will work in browser 
+
+let name = "Harminder"
+//OR
+let name2 = 'Harminder'
+
+//Means string can be declared in both single quote and double quote
+
+// Other Data types
+//Undefined -> Means values is not assigned to a variable yet, it is not null. 
+//ex
+let a;
+
+// Null => null is a data type as well and a standalone value as well
+let b = null // means that b has a value of null
+
+let c = "" // now this not not means null or undefined, coz by using "" we defined that the value of c will be of type still.
+
+// synbol = Data type used to see which component are unique
+// Obejcts
+
+
+console.log(typeof name); //string
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
