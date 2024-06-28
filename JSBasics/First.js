db86d9cdb9cf7948dd7b822d9d19603a7c37a5e9
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-06-28 23:19:16 +0000

    Variables&Const

--- JSBasics/First.js
+++ JSBasics/First.js
@@ -1,1 +1,10 @@
 console.log("Hi Harminder!")
+
+const rollNo = 123456;
+let myName = "Harminder"
+var jonDesc = "Software Developer";
+originCity = "Jalandhar"
+let currentCity;
+
+
+console.table([rollNo, myName, jonDesc, originCity, currentCity])
