7632c446a593337ae3367ccde4661d84631cf5f0
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-06-29 00:10:40 +0000

    operations

---
+++ JSBasics/03_ConversionOperations.js
@@ -0,0 +1,51 @@
+let score = 33
+
+let score2 = Number(score)
+console.log(score2) //33
+console.log(typeof score2) //Number
+
+/*Notes
+
+33 => 33
+"33abc" => NaN (Means the input is not a number when tried with Number())
+true => 1
+null => 0
+undefined => NaN
+
+*/
+
+// we can convert to Boolean as well
+let a = 1
+console.log(Boolean(a)) // true
+console.log(Boolean(0)) // false
+console.log(Boolean("")) // false
+console.log(Boolean("Harminder")) // true
+
+
+// *************************************** Operations******************************************
+
+let value = 3
+let negVal = -value
+
+console.log(value) //3
+console.log(negVal ) //-3
+
+console.log(2+2) //4
+console.log(2-2) //0
+console.log(2*2) //4
+console.log(2**3) //8 meaing 2 ki power 3
+console.log(2/2) //1
+console.log(2%2) //0
+
+
+console.log(1 + "2") // 12
+console.log("1" + "2") // 12
+console.log("1" + 2) // 12
+console.log(1 + 2+ "2") // 32
+console.log("1" + 2+ 2) // 122
+
+
+
+
+
+
