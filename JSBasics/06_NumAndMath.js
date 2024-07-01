82c6dccf71d0f88fb681bc5b9da904323b2aff85
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-07-01 20:17:56 +0000

    mathNum

---
+++ JSBasics/06_NumAndMath.js
@@ -0,0 +1,13 @@
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.2))
+// console.log(Math.round(4.7))
+// console.log(Math.round(4.1238))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+const min = 10
+const max = 20
+
+const range = max -min + 1
+
+console.log(Math.floor( (Math.random() * range) + min ))
