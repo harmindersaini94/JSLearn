a47f2d99f746f5e265466b21613a3baee070c543
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-07-01 19:17:59 +0000

    strings

---
+++ JSBasics/05_StackHeapMemory.js
@@ -0,0 +1,7 @@
+const name = "Harminder"
+const count = 50
+
+console.log("My name is" + name + ", and count is " + count) //OLD Way of doing it
+console.log(`My name is ${name}, and count is ${count}`) //NEW and preferred way
+
+// Using ojbects way to declare String
