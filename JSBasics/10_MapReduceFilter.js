ae708e1d235aafa7f1f941aea91edd26c4fee307
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-07-04 23:10:40 +0000

    filter

---
+++ JSBasics/10_MapReduceFilter.js
@@ -0,0 +1,51 @@
+// DIFFERENCE Between ForEach and Map
+
+//FOR EACH does not return a value, even if you put return as explicit, it even then wont retunr
+
+// MAP do return it
+
+const newArr = ["JS", "JAVA", "C#", "PHP"]
+
+const val = newArr.forEach((item) => {
+    return item
+})
+
+console.log(val)
+
+
+const val2 = newArr.map( (item) => {   // means agar {} toh return word explicitly likhna hoga why because, By using {} hum ek scope create krte hai and that should return a value
+ return item
+})
+console.log(val2)
+
+const val3 = newArr.map( (item) => (  // means agar () toh return nhi likhna hoga
+    item
+))
+   console.log(val3)
+
+
+// FILTEr
+
+const numArr = [1,2,3,4,5,6,7,8,9,10]
+
+const finalVal = numArr.filter( (item) => {
+    return item > 4
+})
+
+console.log(finalVal)
+
+const books = [
+    {title:'Book One', genre:'Fiction', public:1991, edition:2004},
+    {title:'Book Two', genre:'History', public:1992, edition:2005},
+    {title:'Book Three', genre:'Sci-Fi', public:1993, edition:2006},
+    {title:'Book Four', genre:'Fiction', public:1994, edition:2007},
+    {title:'Book Five', genre:'History', public:1995, edition:2008},
+    {title:'Book Six', genre:'Comedy', public:1996, edition:2009}
+]
+
+const fictionData = books.filter((eachBook) => {
+    return eachBook.genre === "Fiction"
+})
+
+console.log(fictionData)
+
