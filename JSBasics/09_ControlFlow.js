041c6c88ba03ed1670986f01bff07410e48c41e8
Harminder Singh <63682981+harmindersaini94@users.noreply.github.com>
2024-07-04 20:38:53 +0000

    loops

---
+++ JSBasics/09_ControlFlow.js
@@ -0,0 +1,16 @@
+for (let a=1; a <= 10; a++){
+    //console.log(a)
+}
+
+let score = 5
+
+do{
+    console.log(score)
+    score++
+} while(score <=10)
+
+///////////////////////////////////////////    //Specialized Loop for Arrays //////////////////////////////////////////////////////
+
+
+
+    
